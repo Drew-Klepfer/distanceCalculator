15c305327f71df9191bb44cb8ea6ecf98bd54feb
Drew-Klepfer <klepfed@wwu.edu>
2023-06-15 19:24:02 -0700

    fixed calculation

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const haversine = require('haversine-distance');
 const Coordinates = require('coordinate-parser');
-/*  const a = { latitude: 37.8136, longitude: 144.9631 }
-    const b = { latitude: 33.8650, longitude: 151.2094 }        
-    console.log(haversine(a, b)) // 714504.18 (in meters) */
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 const path = require('path')
 
@@ -15,9 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* set view engine */
 app.set("view engine", "pug");
-app.set('views', path.join( __dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
 });
 
@@ -25,35 +22,61 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+/* Coordinate calculation */
 app.post("/", (req, res) => {
     const pointA = req.body.pointA;
     const pointB = req.body.pointB;
-    
+
     const regexExp = /^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$/gi;
 
-    if(!regexExp.test(pointA) || !regexExp.test(pointB)){
+    if (!regexExp.test(pointA) || !regexExp.test(pointB)) {
         res.sendFile(__dirname + '/public/index.html');
     }
 
     position1 = new Coordinates(pointA);
     position2 = new Coordinates(pointB);
 
-    var kilometers = findDistance(position1, position2);
+    const kilometers = findDistance(position1, position2);
+    Math.round(kilometers/1000);
+
+    console.log(kilometers);
 
     res.render('index', {
         kilo: kilometers
     });
+});
 
-    //res.send(kilometers);
+/* Address calculation */
+app.post("/address", (req, res) => {
+    const address1 = req.body.address1;
+    const address2 = req.body.address2;
 });
 
 /* calculate distance between two coordinates */
 function findDistance(a, b) {
-    const A = [a.latitude, a.longitude];
-    const B = [b.latitude, b.longitude];
+    const lat1 = a.latitude;
+    const lat2 = b.latitude;
+    const lon1 = a.longitude;
+    const lon2 = b.longitude;
 
-    var haversine_m = haversine(A, B); // meters
-    var haversine_km = haversine_m / 1000;
-
-    return haversine_km;
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+        dist = dist.toFixed(3);
+        console.log(dist);
+        return dist;
+    }
 }
